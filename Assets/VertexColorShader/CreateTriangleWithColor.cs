a7e579bd905b171712da63b6465813691cc3a58b
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2018-08-25 15:01:24 +0900

    Add vertex color shader

---
+++ Assets/VertexColorShader/CreateTriangleWithColor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateTriangleWithColor : MonoBehaviour {
+
+	void Start () {
+		var mesh = new Mesh ();
+
+		var vertices = new List<Vector3> {
+			new Vector3 (-1, 0, 0),
+			new Vector3 (0, 1, 0),
+			new Vector3 (1, 0, 0),
+		};
+		mesh.SetVertices (vertices);
+
+		var triangles = new List<int> { 0, 1, 2 };
+		mesh.SetTriangles (triangles, 0);
+
+		var colors = new List<Color> {
+			Color.red,
+			Color.blue,
+			Color.green,
+		};
+		mesh.SetColors (colors);
+
+		var meshFilter = GetComponent<MeshFilter> ();
+		meshFilter.mesh = mesh;
+	}
+
+}
---
+++ Assets/VertexColorShader/VertexColor.shader
@@ -0,0 +1,37 @@
+﻿Shader "MyShader/VertexColor"
+{
+	SubShader
+	{
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float4 color: COLOR;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+				float4 color : COLOR;
+			};
+
+			void vert (in appdata v, out v2f o)
+			{
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.color = v.color;
+			}
+
+			void frag (in v2f i, out float4 col : SV_Target)
+			{
+				col = i.color;
+			}
+			ENDCG
+		}
+	}
+}
